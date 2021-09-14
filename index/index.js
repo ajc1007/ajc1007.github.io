6d7ba18962265fe92f42684ca5be357e7ba82fc3
ajc1007 <ajcitro02@gmail.com>
2021-09-14 19:22:58 -0400

    V0.3.2
    Launch information updated, new features

---
+++ index/index.js
@@ -0,0 +1,27 @@
+//set countdown date
+	var countDownDate = new Date("September 15, 2021 20:02").getTime();
+
+//update count every second
+	var x = setInterval(function() {
+	
+//get today's date
+	var now = new Date().getTime();
+
+//get distance between now and target date
+	var distance = countDownDate - now;
+
+//calculations for days, hours, minutes, seconds
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (1000 * 60)) / (1000));
+
+//display result in the element with id=countdown
+	document.getElementById("countdown").innerHTML = days + " Days " + hours + " Hours " + minutes + " Minues " + seconds + " Seconds ";
+
+//if countdown is finished, display text
+	if (distance < 0) {
+		clearInterval(x);
+		document.getElementById("countdown").innerHTML = "Launch! Standby for next launch countdown"
+	}
+}, 1000);
